f39e15a83c082a0b7212336d7bde6951a42b631b
b9004308 <b9004308@newcastle.ac.uk>
2021-01-08 20:02:58 +0000

    compressed code

--- machinelearning/frauddetection/checktransaction.py
+++ machinelearning/frauddetection/checktransaction.py
@@ -1,10 +1,22 @@
 import numpy as np
 from sklearn.neighbors import LocalOutlierFactor
-from getdata import getdata
+import pandas as pd
+
+
+
+def getdata():
+    df = pd.read_csv('1.csv', parse_dates=['date'])
+    df = df.sort_values(by='date',ascending=False)
+    for index, row in df.iterrows():
+        if row['amount'] ==0:
+            df.drop(index, inplace=True)
+    g = df.groupby(pd.Grouper(key='recipient'))
+    dfs = [group for _,group in g]
+    dfs = dfs[::-1]
+    return dfs
+
 
 df = getdata()
-dates = df[1]
-df = df[0]
 fraudinput = [["Tesco"],[[125]]]
 anomaly=False
 for i in df:
@@ -17,8 +29,8 @@ for i in df:
             array = array.reshape(array.shape[0], -1)
             cls = LocalOutlierFactor(n_neighbors=2, novelty=True)
             cls.fit(array)
-            isoutlier2 = cls.decision_function(fraudinput[1])
-            realnum = isoutlier2 / -10000000000
+            isoutlier = cls.decision_function(fraudinput[1])
+            realnum = isoutlier / -10000000000
             if realnum > 100:
                 anomaly=True
             print(realnum)
