a19121dcccb3a8625eb6fa97c001d962f4711a24
b9004308 <b9004308@newcastle.ac.uk>
2021-01-11 10:46:24 +0000

    commented code

--- machinelearning/predictmonthlyspending/predict_spending.py
+++ machinelearning/predictmonthlyspending/predict_spending.py
@@ -1,56 +1,98 @@
-import matplotlib.pyplot as plt
-from pandas import DataFrame
+# import libraries
+from dateutil.relativedelta import relativedelta
 from sklearn import linear_model
 import datetime as dt
-from getdata import getdata
 import pandas as pd
+from pandas import DataFrame
+import datetime
 
 
-df = getdata()
-dates=df[1]
-df=df[0]
-dates["date"]=pd.to_datetime(dates["date"])
-totals=[]
-for i in df:
-    recipient=i["recipient"].iloc[0]
-    frames=[i,dates]
-    i=pd.concat(frames)
-    i = i.sort_values(by='date',ascending=False)
-    subdf = DataFrame (i,columns=['recipient','date','amount'])
-    g = subdf.groupby(pd.Grouper(key='date',freq="M"))
-    dfs = [group for _,group in g]
-    monthtotals=[]
-    for j in dfs:
-        monthtotals.append([j["date"].iloc[0],j['amount'].sum()])
-    seperated = DataFrame (monthtotals,columns=['date','amount'])
-    X = pd.DataFrame(seperated['date'])
-    X["date"]=pd.to_datetime(X["date"])
-    X["date"]=X["date"].map(dt.datetime.toordinal)
-    y = pd.DataFrame(seperated['amount'])
-    # Make predictions using the testing set
-    linearregression=linear_model.LinearRegression()
-    linearregression.fit(X,y)
-    nextmonth=X["date"].iloc[-1]+30
-    predicted = linearregression.predict([[nextmonth]])[-1][0]
-    if predicted<0:
-        predicted=0
-    print(predicted)
-    #polynomial regression
-    # poly_reg = PolynomialFeatures(degree=5)
-    # X_poly = poly_reg.fit_transform(X)
-    # pol_reg = LinearRegression()
-    # pol_reg.fit(X_poly, y)
-    # polyresult=pol_reg.predict(poly_reg.fit_transform([[nextmonth]]))[0][0]
-    # print("poly",polyresult)
-    # print("linear",predicted)
-    # polynomialgraph=seperated.append({'date' : nextmonth , 'amount' : polyresult} , ignore_index=True)
-    # polynomialgraph.plot(x ='date', y='amount', kind ='line',marker='x',title=recipient+" poly")
-    # plt.show()
-    predictiongraph=seperated.append({'date' : nextmonth , 'amount' : predicted} , ignore_index=True)
-    predictiongraph.plot(x ='date', y='amount', kind ='line',marker='x',title=recipient+" predicted")
-    plt.show()
-    totals.append([recipient,predicted])
-total=0
-for i in totals:
-    total+=i[1]
-print("Total expected spending is :"+str(total))
+def getdata():
+    # read in data from csv and sort by data
+    df = pd.read_csv('1.csv', parse_dates=['date'])
+    df = df.sort_values(by='date', ascending=False)
+    # get first and last date
+    firstdate = df["date"].iloc[-1]
+    lastdate = df["date"].iloc[0]
+    # generate array of dates and put it in a data frame
+    dates = gendates(firstdate, lastdate)
+    dates = DataFrame(dates, columns=['recipient', 'date', 'amount'])
+    # remove empty purchases in csv
+    for index, row in df.iterrows():
+        if row['amount'] == 0:
+            df.drop(index, inplace=True)
+    # group by recipient and flip order
+    g = df.groupby(pd.Grouper(key='recipient'))
+    dfs = [group for _, group in g]
+    dfs = dfs[::-1]
+    return dfs, dates
+
+
+def gendates(startdate, enddate):
+    # format start and end dates
+    sd = [startdate.year, startdate.month, startdate.day]
+    ed = [enddate.year, enddate.month, enddate.day]
+    #  convert start and end dates to datetime
+    dt = datetime.datetime(sd[0], sd[1], sd[2])
+    end = datetime.datetime(ed[0], ed[1], ed[2])
+    # making it so it makes a new date for every day
+    step = datetime.timedelta(days=1)
+    result = []
+    # loop through dates and add empty amount and date
+    while dt < end:
+        result.append(["", dt.strftime('%Y-%m-%d'), 0.00])
+        dt += step
+    return (result)
+
+
+def predict():
+    # get dates and transactions
+    df = getdata()
+    dates = df[1]
+    df = df[0]
+    # convert dates to datetime format
+    dates["date"] = pd.to_datetime(dates["date"])
+    totals = []
+    # loop through each recipient
+    for i in df:
+        # get recipient name
+        recipient = i["recipient"].iloc[0]
+        # combine dates and the original datafram
+        frames = [i, dates]
+        i = pd.concat(frames)
+        # sort datafram by date
+        i = i.sort_values(by='date', ascending=False)
+        # group transactions by month
+        subdf = DataFrame(i, columns=['recipient', 'date', 'amount'])
+        g = subdf.groupby(pd.Grouper(key='date', freq="M"))
+        dfs = [group for _, group in g]
+        # initialise month totals
+        monthtotals = []
+        # loop through transactions in month and total them
+        for j in dfs:
+            monthtotals.append([j["date"].iloc[0], j['amount'].sum()])
+        # create new data frame with months and their totals
+        seperated = DataFrame(monthtotals, columns=['date', 'amount'])
+        # set date as X
+        X = pd.DataFrame(seperated['date'])
+        X["date"] = pd.to_datetime(X["date"])
+        # get the month to predict
+        nextmonth = X["date"].iloc[-1] + relativedelta(months=+1)
+        # convert dates to ordinal
+        X["date"] = X["date"].map(dt.datetime.toordinal)
+        nextmonth = nextmonth.toordinal()
+        # set y as amount spent
+        y = pd.DataFrame(seperated['amount'])
+        # initialise linear regression model and train it
+        linear_regression = linear_model.LinearRegression()
+        linear_regression.fit(X, y)
+        # predict the spending in the next month
+        predicted = linear_regression.predict([[nextmonth]])[-1][0]
+        # check if it predicts a negative number, if it does set as 0
+        if predicted < 0:
+            predicted = 0
+        # append to totals
+        totals.append([recipient, predicted])
+    # return totals and their name
+    return totals
+
